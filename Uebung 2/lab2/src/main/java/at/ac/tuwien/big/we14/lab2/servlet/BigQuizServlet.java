9025d7644a730a7a499da408b4d84facc171d9f7
Romana Jakob <romana.jakob@gmx.at>
2014-04-19 11:09:18 +0200

    Java Server Pages angelegt (noch nicht fertig implementiert) Projekt nochmal neu erstellt, damit Warnings verschwinden

--- Uebung 2/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
+++ Uebung 2/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
@@ -1,8 +1,43 @@
 package at.ac.tuwien.big.we14.lab2.servlet;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import at.ac.tuwien.big.we14.lab2.api.Category;
+import at.ac.tuwien.big.we14.lab2.api.QuestionDataProvider;
+import at.ac.tuwien.big.we14.lab2.api.QuizFactory;
+import at.ac.tuwien.big.we14.lab2.api.impl.ServletQuizFactory;
+
 // Your Servlet implementation
 
-public class BigQuizServlet{
+public class BigQuizServlet extends HttpServlet{
 	
+	// ServletContext coming from javax.servlet.GenericServlet or subclass
+	ServletContext servletContext = getServletContext();
+	QuizFactory factory = ServletQuizFactory.init(servletContext);
+	QuestionDataProvider provider = factory.createQuestionDataProvider();
+	List<Category> categories = provider.loadCategoryData();
+	// category has name and holds questions
+	// questions have attributes and choices
 	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	@Override
+	public String getServletInfo() {
+		return "Handles BigQuizServlet Requests";
+	}
 }
--- Uebung 2/lab2/src/main/webapp/start.jsp
+++ Uebung 2/lab2/src/main/webapp/start.jsp
@@ -1,16 +1,15 @@
-<% @ page import="java.util.*" %>
-
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE html>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de">
-    <head>
+	<head>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Business Informatics Group Quiz - Start</title>
         <link rel="stylesheet" type="text/css" href="style/screen.css" />
         <script src="js/jquery.js" type="text/javascript"></script>
         <script src="js/framework.js" type="text/javascript"></script>
-    </head>
+	</head>
     <body id="startpage">
         <header role="banner" aria-labelledby="mainheading"><h1 id="mainheading"><span class="accessibility">Business Informatics Group</span> Quiz</h1></header>
         <nav role="navigation" aria-labelledby="navheading">
@@ -25,8 +24,6 @@
         </section>
 
         <!-- footer -->
-        <footer role="contentinfo">Â© 2014 BIG Quiz</footer>
+        <footer role="contentinfo">© 2014 BIG Quiz</footer>
     </body>
 </html>
-
-
